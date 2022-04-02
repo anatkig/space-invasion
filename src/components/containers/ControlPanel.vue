42395672e24e24b8e0e0e30954328e16b27f37ff
Anatolii KIGURADZE <AKIGUR26@neptune.dkcorp.net>
2022-04-02 08:59:59 +0600

    worked on control pause and home buttons and their logic

--- src/components/containers/ControlPanel.vue
+++ src/components/containers/ControlPanel.vue
@@ -3,6 +3,14 @@ import { useLivesStore } from "../../stores/lives";
 import { useBulletsLeftStore } from "../../stores/bulletsLeft";
 import { useInvadersDestroyedStore } from "../../stores/invadersDestroyed";
 import { useLevelStore } from "../../stores/level";
+import { reactive } from "vue";
+import { RouterLink, useRouter } from "vue-router";
+
+const state = reactive({
+  pauseResume: "pause/resume",
+});
+
+const router = useRouter();
 
 const lives = useLivesStore();
 const bulletsLeft = useBulletsLeftStore();
@@ -11,39 +19,80 @@ const level = useLevelStore();
 </script>
 
 <template>
-  <div class="control-panel-container">
-    <div class="control-panel-child number-of-lives">
-      lives: {{ lives.$state.lives }}
+  <div class="control-panel">
+    <div class="control-panel_data-container">
+      <div class="control-panel_child number-of-lives">
+        lives: {{ lives.$state.lives }}
+      </div>
+      <div class="control-panel_child level">
+        level: {{ level.$state.level }}
+      </div>
+      <div class="control-panel_child bullets-left">
+        bullets: {{ bulletsLeft.$state.bulletsLeft }}
+      </div>
+      <div class="control-panel_child invader-killed">
+        invaders destroyed: {{ invadersDestroyed.$state.invadersDestroyed }}
+      </div>
     </div>
-    <div class="control-panel-child level">level: {{ level.$state.level }}</div>
-    <div class="control-panel-child bullets-left">
-      bullets: {{ bulletsLeft.$state.bulletsLeft }}
-    </div>
-    <div class="control-panel-child invader-killed">
-      invaders destroyed: {{ invadersDestroyed.$state.invadersDestroyed }}
+    <div class="control-panel_navigation-container">
+      <button
+        class="control-panel_child navigation_child home-button"
+        @click="router.push('/')"
+      >
+        <RouterLink to="/">Home</RouterLink>
+      </button>
+
+      <button class="control-panel_child navigation_child pause-button">
+        {{ state.pauseResume }}
+      </button>
     </div>
   </div>
 </template>
 
 <style>
-.control-panel-container {
-  color: white;
+.control-panel {
+  display: flex;
+  justify-content: space-between;
+
   height: 10vh;
   border-style: solid;
   border-width: 2px 0px;
   border-color: red;
 
+  min-width: 0;
+}
+.control-panel_data-container {
+  color: white;
+
   display: flex;
   justify-content: space-evenly;
 }
-.control-panel-child {
+.control-panel_child {
   border-style: solid;
   border-width: 1px;
   border-color: red;
 
-  display: inline-block;
   padding: 0% 3%;
 
   text-align: center;
+}
+
+.control-panel_navigation-container {
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-end;
+
+  color: black;
+}
+
+.navigation_child {
+  cursor: pointer;
+  font-weight: 700;
+}
+.home-button a {
+  font-weight: inherit !important;
+}
+.navigation_child:hover {
+  transform: scale(1.02);
 }
 </style>
