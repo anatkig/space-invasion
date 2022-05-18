89a32f74f027eea09214045e9a8b4814374fd6b3
Anatolii Kiguradze <v-akiguradze@microsoft.com>
2022-05-18 15:08:00 +0600

    created a window which shows that a life has been taken

--- src/logic/invadersAttack.js
+++ src/logic/invadersAttack.js
@@ -1,4 +1,4 @@
-import { computed } from "vue";
+import { computed } from 'vue';
 
 const invadersAttack = (
   invadersStore,
@@ -17,30 +17,35 @@ const invadersAttack = (
 
   const invaderCycle = setInterval(
     () => {
-      if (startPauseResume.value === "Pause") {
+      if (startPauseResume.value === 'Pause') {
         invadersStore.addInvader({
-          id: Date.now(),
+          id: Date.now()
         });
         const invaders = computed(() =>
-          Array.from(document.querySelectorAll(".invader"))
+          Array.from(document.querySelectorAll('.invader'))
         );
         if (
           invaders.value.some((invader) => {
             return (
               invader.offsetHeight + invader.offsetTop ===
-              document.querySelector(".battle-field").offsetHeight
+              document.querySelector('.battle-field').offsetHeight
             );
           })
         ) {
           if (lives.$state.lives > 1) {
             lives.subtractLives(1);
             invadersStore.removeAllInvaders();
+            modalText.value = 'One life has been taken!';
+            modal.value = !modal.value;
+            setTimeout(() => {
+              modal.value = !modal.value;
+            }, 3000);
           } else {
             clearInterval(invaderCycle);
             lives.subtractLives(1);
-            modalText.value = "Game Over!";
+            modalText.value = 'Game Over!';
             modal.value = !modal.value;
-            startPauseResume.value = "Restart";
+            startPauseResume.value = 'Restart';
           }
         }
       }
