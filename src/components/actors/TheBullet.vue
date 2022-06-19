7d7b631c23209ae9fb7f6a9a6c4570e68e33c1b4
Anatolii Kiguradze <v-akiguradze@microsoft.com>
2022-06-19 11:17:36 +0600

    incorporated bullet logic into TheBullet component

--- src/components/actors/TheBullet.vue
+++ src/components/actors/TheBullet.vue
@@ -1,11 +1,10 @@
 <script setup>
-import { onMounted, reactive } from "vue";
-import bulletLogic from "../../logic/bulletLogic.js";
-import { useBulletsStore } from "../../stores/bullets.js";
-import { useInvadersStore } from "../../stores/invaders.js";
-import { useBulletsLeftStore } from "../../stores/bulletsLeft";
-import { useInvadersDestroyedStore } from "../../stores/invadersDestroyed.js";
-import { useLevelStore } from "../../stores/level.js";
+import { onMounted, reactive, computed } from 'vue';
+import { useBulletsStore } from '../../stores/bullets.js';
+import { useInvadersStore } from '../../stores/invaders.js';
+import { useBulletsLeftStore } from '../../stores/bulletsLeft';
+import { useInvadersDestroyedStore } from '../../stores/invadersDestroyed.js';
+import { useLevelStore } from '../../stores/level.js';
 
 const bulletsStore = useBulletsStore();
 const invadersStore = useInvadersStore();
@@ -13,21 +12,50 @@ const bulletsLeftStore = useBulletsLeftStore();
 const invadersDestroyedStore = useInvadersDestroyedStore();
 const levelStore = useLevelStore();
 
-const { coordinateX, id } = defineProps(["coordinateX", "id"]);
+const { coordinateX, id } = defineProps(['coordinateX', 'id']);
 const bullTop = reactive({
-  bullTop: document.querySelector(".battle-field").offsetHeight - 60,
+  bullTop: document.querySelector('.battle-field').offsetHeight - 60
 });
 
 onMounted(() => {
-  bulletLogic(
-    bulletsStore,
-    invadersStore,
-    invadersDestroyedStore,
-    bulletsLeftStore,
-    levelStore,
-    bullTop
-  );
+  bulletLogic();
 });
+
+const bulletLogic = () => {
+  const topMovement = setInterval(() => {
+    bullTop.bullTop -= 3;
+    const bullets = computed(() =>
+      Array.from(document.querySelectorAll('.bullet'))
+    );
+    const invaders = computed(() =>
+      Array.from(document.querySelectorAll('.invader'))
+    );
+
+    bullets.value.forEach((bullet) =>
+      invaders.value.forEach((invader) => {
+        if (
+          bullet.offsetTop >= invader.offsetTop &&
+          bullet.offsetTop <= invader.offsetTop + invader.offsetHeight &&
+          bullet.offsetLeft >= invader.offsetLeft &&
+          bullet.offsetLeft <= invader.offsetLeft + invader.offsetWidth
+        ) {
+          bulletsStore.removeBullet(bullet.id);
+          invadersStore.removeInvader(invader.id);
+          invadersDestroyedStore.addInvadersDestroyed(1);
+          if (invadersDestroyedStore.$state.invadersDestroyed % 10 === 0) {
+            levelStore.addLevel();
+            bulletsLeftStore.addBulletsLeft(100);
+          }
+        }
+      })
+    );
+
+    if (bullTop.bullTop <= -10) {
+      clearInterval(topMovement);
+      bullets.value.length && bulletsStore.removeBullet(bullets.value[0].id);
+    }
+  }, 1);
+};
 </script>
 
 <template>
