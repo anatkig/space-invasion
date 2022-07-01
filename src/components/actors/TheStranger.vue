c0745c4e282c69efbbd5498db52d13a92bf8559c
anatkig <keeggooggle@gmail.com>
2022-07-01 11:58:18 +0600

    added functionality to different strangers

--- src/components/actors/TheStranger.vue
+++ src/components/actors/TheStranger.vue
@@ -1,24 +1,42 @@
 <script setup>
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import { useLevelStore } from '../../stores/level';
 import { useLivesStore } from '../../stores/lives';
 import { useBulletsLeftStore } from '../../stores/bulletsLeft';
+import { useInvadersStore } from '../../stores/invaders';
+import { useInvadersDestroyedStore } from '../../stores/invadersDestroyed';
 
 import cartridge from '../../assets/cartridge.svg'
+import bomb from '../../assets/bomb.svg';
+import firstAid from '../../assets/firstAid.svg';
 
 const level = useLevelStore().$state.level;
-const lives = useLivesStore().$state.lives;
 const bulletsLeft = useBulletsLeftStore();
+const lives = useLivesStore();
+
+const imageIndex = ref(0);
+
+const images = [
+    cartridge,
+    bomb,
+    firstAid
+];
 
 
 const state = reactive({
     invTop: 0,
     randomCoordinateX: 0,
     speed: Math.floor(30 / level + 20),
-    dislay: "none"
+    dislay: "none",
+    image: images[imageIndex.value]
 });
 
+
 const componetLifecycle = () => {
+    const randomIndex = Math.floor(Math.random() * 3);
+    state.image = images[randomIndex];
+    imageIndex.value = randomIndex;
+
     state.randomCoordinateX = Math.floor(Math.random() *
         (document.querySelector('.battle-field')?.offsetWidth - 70));
     const interval = setInterval(() => {
@@ -55,15 +73,26 @@ const invTopValue = () => {
                     clearInterval(topMovement);
                     state.dislay = "none";
                     state.invTop = 0;
+
+                    if (imageIndex.value === 0) {
+                        bulletsLeft.addBulletsLeft(100);
+                    } else if (imageIndex.value === 1) {
+                        lives.subtractLives(1);
+                        const invaders = useInvadersStore();
+                        const invadersDestroyed = useInvadersDestroyedStore();
+                        invadersDestroyed.addInvadersDestroyed(invaders.$state.invaders.length);
+                        invaders.removeAllInvaders();
+                    } else if (imageIndex.value === 2) {
+                        lives.addLives(1)
+                    }
                     if (lives !== 0) {
                         componetLifecycle();
                     }
-                    bulletsLeft.addBulletsLeft(100);
                 }
             }
             if (
                 state.invTop ===
-                document.querySelector('.battle-field').offsetHeight + 100 || lives === 0
+                document.querySelector('.battle-field').offsetHeight + 100 || lives.$state.lives === 0
             ) {
                 clearInterval(topMovement);
                 state.dislay = "none";
@@ -90,7 +119,7 @@ onMounted(() => {
         top: state.invTop + 'px',
         display: state.dislay
     }">
-        <img :src="cartridge">
+        <img :src="state.image">
     </div>
 </template>
 
